b564dd8ca70b8057e10570c11bde4a0b9cd63e09
Brian Cheong <cheongb002@gmail.com>
2024-12-05 12:07:26 -0500

    add seq flip prob to config

--- projects/configs/sparse4dv3_temporal_r50_1x8_bs6_256x704.py
+++ projects/configs/sparse4dv3_temporal_r50_1x8_bs6_256x704.py
@@ -412,7 +412,7 @@ train_dataloader = dict(
     num_workers=16,
     persistent_workers=True,
     sampler=dict(type='DefaultSampler'),
-    batch_sampler=dict(type='TrackSampler3D', shuffle=True, clip_len=20),
+    batch_sampler=dict(type='TrackSampler3D', shuffle=True, clip_len=10, seq_flip_prob=0.1),
     collate_fn=dict(type='default_collate'),
     dataset=dict(
         **data_basic_config,
