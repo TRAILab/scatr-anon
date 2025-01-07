c3c397c0732baeba05953a77066969b7bcbd8e87
Brian Cheong <cheongb002@gmail.com>
2025-01-07 16:21:27 -0500

    update dataset configuration to use nus-3d-track-cam.py

--- projects/configs/sparse4dv3_temporal_r50_1x8_bs6_256x704.py
+++ projects/configs/sparse4dv3_temporal_r50_1x8_bs6_256x704.py
@@ -1,6 +1,6 @@
 _base_ = [
     './_base_/default_runtime.py',
-    './_base_/datasets/nus-3d-track.py']
+    './_base_/datasets/nus-3d-track-cam.py']
 
 # ================ base config ===================
 plugin = True
