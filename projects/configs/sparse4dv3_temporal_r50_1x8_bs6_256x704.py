9bfedd61cadc6e2236e315093f0c8e823ef46ceb
Brian Cheong <cheongb002@gmail.com>
2025-01-07 21:12:38 -0500

    refactor config to have model in separate file

--- projects/configs/sparse4dv3_temporal_r50_1x8_bs6_256x704.py
+++ projects/configs/sparse4dv3_temporal_r50_1x8_bs6_256x704.py
@@ -1,6 +1,7 @@
 _base_ = [
     './_base_/default_runtime.py',
-    './_base_/datasets/nus-3d-track-cam.py']
+    './_base_/datasets/nus-3d-track-cam.py',
+    './_base_/models/sparse4dv3_temporal_r50.py',]
 
 # ================ base config ===================
 plugin = True
@@ -21,7 +22,6 @@ load_from = None
 resume_from = None
 
 tracking_test = True
-tracking_threshold = 0.2
 class_names = [
     "car",
     "truck",
@@ -36,165 +36,21 @@ class_names = [
 ]
 # ================== model ========================
 
-embed_dims = 256
-num_groups = 8
-num_decoder = 6
-num_single_frame_decoder = 1
-use_deformable_func = True  # mmdet3d_plugin/ops/setup.py needs to be executed
 strides = [4, 8, 16, 32]
 num_levels = len(strides)
 num_depth_layers = 3
-drop_out = 0.1
-temporal = True
-decouple_attn = True
-with_quality_estimation = True
 
 model = dict(
-    type="Sparse4D",
-    use_grid_mask=True,
-    use_deformable_func=use_deformable_func,
-    img_backbone=dict(
-        type="mmdet.ResNet",
-        depth=50,
-        num_stages=4,
-        frozen_stages=-1,
-        norm_eval=False,
-        style="pytorch",
-        with_cp=True,
-        out_indices=(0, 1, 2, 3),
-        norm_cfg=dict(type="BN", requires_grad=True),
-        init_cfg=(
-            dict(type="Pretrained", checkpoint="ckpts/resnet50-19c8e357.pth"),
-        )
-    ),
-    img_neck=dict(
-        type="mmdet.FPN",
-        num_outs=num_levels,
-        start_level=0,
-        out_channels=embed_dims,
-        add_extra_convs="on_output",
-        relu_before_extra_convs=True,
-        in_channels=[256, 512, 1024, 2048],
-    ),
-    depth_branch=dict(  # for auxiliary supervision only
-        type="DenseDepthNet",
-        embed_dims=embed_dims,
-        num_depth_layers=num_depth_layers,
-        loss_weight=0.2,
-    ),
+    img_neck=dict(num_outs=num_levels),
+    depth_branch=dict(num_depth_layers=num_depth_layers),
     pts_bbox_head=dict(
-        type="Sparse4DHead",
-        cls_threshold_to_reg=0.05,
-        decouple_attn=decouple_attn,
-        instance_bank=dict(
-            type="InstanceBank",
-            num_anchor=900,
-            embed_dims=embed_dims,
-            anchor="_nuscenes_kmeans900.npy",
-            anchor_handler=dict(type="SparseBox3DKeyPointsGenerator"),
-            num_temp_instances=600 if temporal else -1,
-            confidence_decay=0.6,
-            feat_grad=False,
-        ),
-        anchor_encoder=dict(
-            type="SparseBox3DEncoder",
-            vel_dims=3,
-            embed_dims=[128, 32, 32, 64] if decouple_attn else 256,
-            mode="cat" if decouple_attn else "add",
-            output_fc=not decouple_attn,
-            in_loops=1,
-            out_loops=4 if decouple_attn else 2,
-        ),
-        num_single_frame_decoder=num_single_frame_decoder,
-        operation_order=(
-            [
-                "gnn",
-                "norm",
-                "deformable",
-                "ffn",
-                "norm",
-                "refine",
-            ]
-            * num_single_frame_decoder
-            + [
-                "temp_gnn",
-                "gnn",
-                "norm",
-                "deformable",
-                "ffn",
-                "norm",
-                "refine",
-            ]
-            * (num_decoder - num_single_frame_decoder)
-        )[2:],
-        temp_graph_model=dict(
-            type="MultiheadAttention",
-            embed_dims=embed_dims if not decouple_attn else embed_dims * 2,
-            num_heads=num_groups,
-            batch_first=True,
-            dropout=drop_out,
-        )
-        if temporal
-        else None,
-        graph_model=dict(
-            type="MultiheadAttention",
-            embed_dims=embed_dims if not decouple_attn else embed_dims * 2,
-            num_heads=num_groups,
-            batch_first=True,
-            dropout=drop_out,
-        ),
-        norm_layer=dict(type="LN", normalized_shape=embed_dims),
-        ffn=dict(
-            type="AsymmetricFFN",
-            in_channels=embed_dims * 2,
-            pre_norm=dict(type="LN"),
-            embed_dims=embed_dims,
-            feedforward_channels=embed_dims * 4,
-            num_fcs=2,
-            ffn_drop=drop_out,
-            act_cfg=dict(type="ReLU", inplace=True),
-        ),
         deformable_model=dict(
-            type="DeformableFeatureAggregation",
-            embed_dims=embed_dims,
-            num_groups=num_groups,
-            num_levels=num_levels,
-            num_cams=6,
-            attn_drop=0.15,
-            use_deformable_func=use_deformable_func,
-            use_camera_embed=True,
-            residual_mode="cat",
-            kps_generator=dict(
-                type="SparseBox3DKeyPointsGenerator",
-                num_learnable_pts=6,
-                fix_scale=[
-                    [0, 0, 0],
-                    [0.45, 0, 0],
-                    [-0.45, 0, 0],
-                    [0, 0.45, 0],
-                    [0, -0.45, 0],
-                    [0, 0, 0.45],
-                    [0, 0, -0.45],
-                ],
-            ),
+            num_levels=num_levels
         ),
         refine_layer=dict(
-            type="SparseBox3DRefinementModule",
-            embed_dims=embed_dims,
             num_cls={{_base_.num_classes}},
-            refine_yaw=True,
-            with_quality_estimation=with_quality_estimation,
         ),
         sampler=dict(
-            type="SparseBox3DTarget",
-            num_dn_groups=5,
-            num_temp_dn_groups=3,
-            dn_noise_scale=[2.0] * 3 + [0.5] * 7,
-            max_dn_gt=32,
-            add_neg_dn=True,
-            cls_weight=2.0,
-            box_weight=0.25,
-            reg_weights=[2.0] * 3 + [0.5] * 3 + [0.0] * 4,
             cls_wise_reg_weights={
                 class_names.index("traffic_cone"): [
                     2.0,
@@ -210,25 +66,10 @@ model = dict(
                 ],
             },
         ),
-        loss_cls=dict(
-            type="mmdet.FocalLoss",
-            use_sigmoid=True,
-            gamma=2.0,
-            alpha=0.25,
-            loss_weight=2.0,
-        ),
         loss_reg=dict(
-            type="SparseBox3DLoss",
-            loss_box=dict(type="mmdet.L1Loss", loss_weight=0.25),
-            loss_centerness=dict(
-                type="mmdet.CrossEntropyLoss", use_sigmoid=True),
-            loss_yawness=dict(type="mmdet.GaussianFocalLoss"),
             cls_allow_reverse=[class_names.index("barrier")],
-        ),
-        decoder=dict(type="SparseBox3DDecoder",
-                     score_threshold=tracking_threshold),
-        reg_weights=[2.0] * 3 + [1.0] * 7,
-    ),
+        )
+    )
 )
 
 # ================== data ========================
