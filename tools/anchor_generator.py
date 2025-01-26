6fce952288f467d0a4a0020e977a6589f215f34e
Brian Cheong <cheongb002@gmail.com>
2025-01-26 14:47:16 -0500

    refactor: replace mmcv with mmengine for data loading in anchor_generator.py

--- tools/anchor_generator.py
+++ tools/anchor_generator.py
@@ -1,7 +1,7 @@
 # (TODO) upgrade this script to MMLAB 2.0 libraries
 import numpy as np
 from sklearn.cluster import KMeans
-import mmcv
+import mmengine
 
 from projects.mmdet3d_plugin.core.box3d import *
 
@@ -13,7 +13,7 @@ def get_kmeans_anchor(
     output_file_name="nuscenes_kmeans900.npy",
     verbose=False,
 ):
-    data = mmcv.load(ann_file, file_format="pkl")
+    data = mmengine.load(ann_file, file_format="pkl")
     gt_boxes = np.concatenate([x["gt_boxes"] for x in data["infos"]], axis=0)
     distance = np.linalg.norm(gt_boxes[:, :3], axis=-1, ord=2)
     mask = distance <= detection_range
