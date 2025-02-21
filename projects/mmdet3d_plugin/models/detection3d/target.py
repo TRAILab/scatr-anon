5261ae1cb2bcca096f16eab0a84763d1bc8f0200
Brian Cheong <cheongb002@gmail.com>
2025-02-21 09:33:39 -0500

    added second chance tq param to target

--- projects/mmdet3d_plugin/models/detection3d/target.py
+++ projects/mmdet3d_plugin/models/detection3d/target.py
@@ -38,6 +38,7 @@ class SparseBox3DTarget(BaseTargetWithDenoising):
         supervise_qc: bool = False,
         point_cloud_range=[-54.0, -54.0, -5.0, 54.0, 54.0, 3.0],
         embed_dims: int = 256,
+        second_chance_tq: bool = False,
         # heatmap target params
         gaussian_overlap: float = 0.1,
         min_radius: int = 2,
@@ -61,8 +62,9 @@ class SparseBox3DTarget(BaseTargetWithDenoising):
         self.embed_dims = embed_dims
         self.gaussian_overlap = gaussian_overlap
         self.min_radius = min_radius
+        self.second_chance_tq = second_chance_tq
 
-    def encode_reg_target(self, box_target: List[LiDARInstance3DBoxes], device=None):
+    def encode_reg_target(self, box_target: List[LiDARInstance3DBoxes], device=None) -> List[torch.Tensor]:
         outputs = []
         for box in box_target:
             output = self.encode_reg_target_single(box, device)
