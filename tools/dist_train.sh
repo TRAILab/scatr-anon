ea1d7116487b73cc01bd8f9263010580efb73a5f
Brian Cheong <cheongb002@gmail.com>
2025-03-03 12:39:10 -0500

    remove master port argument from dist_train.sh

--- tools/dist_train.sh
+++ tools/dist_train.sh
@@ -22,7 +22,6 @@ torchrun \
     --node_rank=$NODE_RANK \
     --master_addr=$MASTER_ADDR \
     --nproc_per_node=$GPUS \
-    --master_port=$PORT \
     $(dirname "$0")/train.py \
     $CONFIG \
     --launcher pytorch ${@:3}
