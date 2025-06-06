9ddcf28c5011c56074625af8cf61b3642ffd06af
Brian Cheong <cheongb002@gmail.com>
2025-06-06 12:58:50 -0400

    modify update_infos for nusc to get cam_instances, incl seg mask

--- tools/dataset_converters/update_infos_to_v2.py
+++ tools/dataset_converters/update_infos_to_v2.py
@@ -368,9 +368,7 @@ def update_nuscenes_infos(pkl_path, out_dir):
                     'instance_inds'][i]
                 empty_instance = clear_instance_unused_keys(empty_instance)
                 temp_data_info['instances'].append(empty_instance)
-            temp_data_info[
-                'cam_instances'] = generate_nuscenes_camera_instances(
-                    ori_info_dict, nusc)
+            temp_data_info['cam_instances'] = ori_info_dict.get('cam_instances', None)
         if 'pts_semantic_mask_path' in ori_info_dict:
             temp_data_info['pts_semantic_mask_path'] = Path(
                 ori_info_dict['pts_semantic_mask_path']).name
