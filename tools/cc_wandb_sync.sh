2d7101e3d55810643952a1754fb0e769f67781b6
Brian Cheong <cheongb002@gmail.com>
2025-06-18 19:22:17 -0400

    sync wand bnewest to oldest

--- tools/cc_wandb_sync.sh
+++ tools/cc_wandb_sync.sh
@@ -3,17 +3,19 @@
 SING_IMG=/home/$USER/projects/rrg-swasland/$USER/singularity/sparse4d-lidar-apptainer-0223.sif
 SYNC_DIR=/home/$USER/job_artifacts/Sparse4D-L/artifacts
 BIND_DIR=/wandb
-module load apptainer
+module load apptainer/1.3.5
 
 while :; do
     date
-    for EXPERIMENT_DIR in "$SYNC_DIR"/*/wandb; do
+    # Sort experiment dirs (wandb subdirs) from newest to oldest
+    for EXPERIMENT_DIR in $(ls -td "$SYNC_DIR"/*/wandb 2>/dev/null); do
         if [ -d "$EXPERIMENT_DIR" ]; then
-            for RUN_DIR in "$EXPERIMENT_DIR"/*; do
+            # Sort run dirs from newest to oldest
+            for RUN_DIR in $(ls -td "$EXPERIMENT_DIR"/* 2>/dev/null); do
                 if [ -d "$RUN_DIR" ]; then
                     echo "Syncing $RUN_DIR"
                     CONTAINER_CMD="apptainer --silent exec --nv -e --pwd /
-                    --env "WANDB_API_KEY=$WANDB_API_KEY"
+                    --env \"WANDB_API_KEY=$WANDB_API_KEY\"
                     --bind=$RUN_DIR:$BIND_DIR
                     $SING_IMG
                     wandb sync $BIND_DIR" # --project JDT3D --entity trailab --job_type cc_narval
