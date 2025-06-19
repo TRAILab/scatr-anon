e210ce87aaa82c3ecb7742f491906ced36638254
Brian Cheong <cheongb002@gmail.com>
2025-06-19 13:25:03 -0400

    update env variable, support iter-based disable track sample

--- projects/mmdet3d_plugin/engine/hooks/disable_track_sample_hook.py
+++ projects/mmdet3d_plugin/engine/hooks/disable_track_sample_hook.py
@@ -10,6 +10,10 @@ from projects.mmdet3d_plugin.datasets import TrackSample
 
 @HOOKS.register_module()
 class DisableTrackSampleHook(DisableObjectSampleHook):
+    def __init__(self, disable_after_epoch:int = 15, disable_after_iter:int = None):
+        super().__init__(disable_after_epoch)
+        self.disable_after_iter = disable_after_iter
+
     def before_train_epoch(self, runner: Runner):
         """Close augmentation.
 
@@ -23,6 +27,44 @@ class DisableTrackSampleHook(DisableObjectSampleHook):
         if is_model_wrapper(model):
             model = model.module
         if epoch == self.disable_after_epoch:
+            runner.logger.info('Disable ObjectSample')
+            dataset = runner.train_dataloader.dataset
+            # handle dataset wrapper
+            if not isinstance(dataset, BaseDataset):
+                dataset = dataset.dataset
+            for transform in dataset.pipeline.transforms:  # noqa: E501
+                if isinstance(transform, TrackSample):
+                    assert hasattr(transform, 'disabled')
+                    transform.disabled = True
+            # The dataset pipeline cannot be updated when persistent_workers
+            # is True, so we need to force the dataloader's multi-process
+            # restart. This is a very hacky approach.
+            if hasattr(train_loader, 'persistent_workers'
+                       ) and train_loader.persistent_workers is True:
+                train_loader._DataLoader__initialized = False
+                train_loader._iterator = None
+                self._restart_dataloader = True
+        else:
+            # Once the restart is complete, we need to restore
+            # the initialization flag.
+            if self._restart_dataloader:
+                train_loader._DataLoader__initialized = True
+
+    def before_train_iter(self, runner, batch_idx, data_batch = None):
+        """Close augmentation.
+
+        Args:
+            runner (Runner): The runner.
+        """
+        if self.disable_after_iter is None:
+            return
+        iter = runner.iter
+        train_loader = runner.train_dataloader
+        model = runner.model
+        # TODO: refactor after mmengine using model wrapper
+        if is_model_wrapper(model):
+            model = model.module
+        if iter == self.disable_after_iter:
             runner.logger.info('Disable ObjectSample')
             dataset = runner.train_dataloader.dataset
             # handle dataset wrapper
